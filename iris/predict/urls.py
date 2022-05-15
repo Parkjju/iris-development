343783c3dd2fe0e759ec37bf4a81959d9f6bd35f
Dabin Park <dabin3178@hufs-gsuite.kr>
2022-05-15 10:29:15 +0900

    predict 초기 설정(템플릿 전까지)

---
+++ iris/predict/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+app_name = 'preict'
+
+urlpatterns = [
+    path('', views.predict, name='predict'),
+]
---
+++ iris/predict/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def predict(request):
+    return render(request, 'predict.html')
