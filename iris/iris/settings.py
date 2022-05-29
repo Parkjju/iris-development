3f060423a67ab6b4b5989dc48de896d11d4d651f
Dabin Park <dabin3178@hufs-gsuite.kr>
2022-05-29 21:12:25 +0900

    클론 코딩 완료git add .

--- iris/iris/settings.py
+++ iris/iris/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'predict',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'iris.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ iris/predict/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class PredResults(models.Model):
+
+    sepal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    petal_length = models.FloatField()
+    petal_width = models.FloatField()
+    classification = models.CharField(max_length=38)
+
+    def __str__(self):
+        return self.classification
