e9eaaf105fd2fdbf0f9cecaa22357a95aa506e92
Parkjju <rudwns3927@gmail.com>
2022-06-06 00:23:09 +0900

    [chore] Add static files

--- predict/views.py
+++ predict/views.py
@@ -17,8 +17,8 @@ import os
 # your project root => absolute path
 # path = "/Users/yoohajun/PycharmProjects/iris_development"
 # path = os.path.join(BASE_DIR)
-knnpath = os.path.join(str(settings.BASE_DIR)+'/knn_model.pkl')
-svcpath = os.path.join(str(settings.BASE_DIR)+'/svc_model.pkl')
+knnpath = os.path.join(str(settings.STATICFILES_DIRS[0])+'/knn_model.pkl')
+svcpath = os.path.join(str(settings.STATICFILES_DIRS[0])+'/svc_model.pkl')
 print(knnpath)
 
 @login_required
